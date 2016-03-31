2b9d38ec122438d7bc5c371bccdb0d90ab89b234
Nicolas Clément <punkryden@gmail.com>
2016-03-31 13:29:13 +0200

    Added files via upload

--- plot2.R
+++ plot2.R
@@ -1,6 +1,8 @@
 ###################################################################
 # Title : plot2.R
-# Description :  
+# Description : Have total emissions from PM2.5 decreased in the 
+# Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
+# Use the base plotting system to make a plot answering this question.  
 # 
 ###################################################################
 
